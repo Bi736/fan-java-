83948b2f01fb406803c009e13cd06f84bad4c38c
Biswajit Sarker <86579868+Bi736@users.noreply.github.com>
2021-09-23 22:41:36 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+let main = document.getElementById('box');
+let fans = document.getElementById('phakha');
+
+function on() {
+  fans.classList.add('moving');
+}
+
+function off() {
+   fans.classList.remove('moving');
+}
